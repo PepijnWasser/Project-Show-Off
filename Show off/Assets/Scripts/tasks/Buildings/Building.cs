d7591d334ddf45ca5f064b7db80f75db4a0dd7e9
Pepijn Wasser <482906@student.saxion.nl>
2021-05-12 15:22:26 +0200

    multiple tasks at a building

--- Show off/Assets/Scripts/tasks/Buildings/Building.cs
+++ Show off/Assets/Scripts/tasks/Buildings/Building.cs
@@ -1,21 +1,69 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Building : MonoBehaviour
 {
-    public bool active;
     public bool showMenu;
+    public bool active;
     public GameObject menu;
+    public GameObject menuPanel;
+    public GameObject taskManager;
+    public GameObject AcceptButtonPrefab;
+
+    public List<Task> taskAtThisLocation = new List<Task>();
+    public List<GameObject> placedListObjects = new List<GameObject>();
+
+    private void Awake()
+    {
+        TaskManager.onCurrentTasksChanged += UpdatePanels;
+    }
+
+    private void OnDestroy()
+    {
+        TaskManager.onCurrentTasksChanged -= UpdatePanels;
+    }
 
     void Update()
     {
-        CheckColor();
         CheckClicked();
         ShowMenu();
     }
 
-    void CheckColor()
+    void UpdatePanels()
+    {
+        UpdateTaskList();
+        CheckIfActive();
+        UpdateColor();
+        DestroyAllPlacedPrefabs();
+        GenerateListPrefabs();
+    }
+
+    void UpdateTaskList()
+    {
+        taskAtThisLocation.Clear();
+        foreach (Task task in taskManager.GetComponent<TaskManager>().currentTasks)
+        {
+            if (task.placeOfQuest.ToString() == this.tag)
+            {
+                taskAtThisLocation.Add(task);
+            }
+        }
+    }
+    void CheckIfActive()
+    {
+        if (taskAtThisLocation.Count > 0)
+        {
+            active = true;
+        }
+        else
+        {
+            active = false;
+        }
+    }
+
+    void UpdateColor()
     {
         if (active)
         {
@@ -29,7 +77,7 @@ public class Building : MonoBehaviour
 
     void CheckClicked()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && active)
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
@@ -43,16 +91,19 @@ public class Building : MonoBehaviour
                 else
                 {
                     showMenu = false;
-                    Debug.Log(hit.collider.name + " " + this.name);
                 }
             }
+        }
+        else if(active == false)
+        {
+            showMenu = false;
         }
     }
     void ShowMenu()
     {
         if(menu != null)
         {
-            if (showMenu && active)
+            if (showMenu)
             {
                 menu.SetActive(true);
             }
@@ -61,5 +112,27 @@ public class Building : MonoBehaviour
                 menu.SetActive(false);
             }
         }      
+    }
+
+    void DestroyAllPlacedPrefabs()
+    {
+        foreach(GameObject obj in placedListObjects)
+        {
+            Destroy(obj);
+        }
+        placedListObjects.Clear();
+    }
+
+    void GenerateListPrefabs()
+    {
+        if(AcceptButtonPrefab != null)
+        {
+            foreach (Task task in taskAtThisLocation)
+            {
+                GameObject newObject = Instantiate(AcceptButtonPrefab, menuPanel.transform);
+                newObject.GetComponentInChildren<Text>().text = task.name;
+                placedListObjects.Add(newObject);
+            }
+        }
     }
 }
