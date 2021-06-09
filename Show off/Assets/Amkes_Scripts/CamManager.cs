890a47d6cbb3f7e7a2c263a6b33e3823dd3ba13a
Pepijn Wasser <482906@student.saxion.nl>
2021-06-09 16:14:34 +0200

    changed camManager

--- Show off/Assets/Amkes_Scripts/CamManager.cs
+++ Show off/Assets/Amkes_Scripts/CamManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ClickManager : MonoBehaviour
+public class CamManager : MonoBehaviour
 {
     private float clicked = 0.0f;
     private float clickTime = 0.0f;
@@ -21,12 +21,10 @@ public class ClickManager : MonoBehaviour
     public GameObject CropFarmCam;
     public GameObject MerchFactoryCam;
 
-    private void Update()
+    void Update()
     {
-        if (DoubleClick())
+        if (Input.GetMouseButtonDown(0))
         {
-            Debug.Log("Double-click detected");
-
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
@@ -34,67 +32,61 @@ public class ClickManager : MonoBehaviour
             {
                 if (hit.transform.name == "Taskboard")
                 {
-                    Debug.Log("Clicked on Taskboard");
+                    //Debug.Log("Clicked on Taskboard");
                     //Set Taskboard-cam active
-                    mainCam.SetActive(false);
-                    TaskboardCam.SetActive(true);
+                    ActivateCamera(TaskboardCam);
 
                     //Disable camera control
                     cameraControllerScript.enabled = false;
                 }
                 if (hit.transform.name == "Shop")
                 {
-                    Debug.Log("Clicked on Shop");
+                    //Debug.Log("Clicked on Shop");
                     //Set Taskboard-cam active
-                    mainCam.SetActive(false);
-                    ShopCam.SetActive(true);
+                    ActivateCamera(ShopCam);
 
                     //Disable camera control
                     cameraControllerScript.enabled = false;
                 }
                 if (hit.transform.name == "Lab")
                 {
-                    Debug.Log("Clicked on Lab");
+                    //Debug.Log("Clicked on Lab");
                     //Set Taskboard-cam active
-                    mainCam.SetActive(false);
-                    LabCam.SetActive(true);
+                    ActivateCamera(LabCam);
 
                     //Disable camera control
                     cameraControllerScript.enabled = false;
                 }
                 if (hit.transform.name == "Harbor")
                 {
-                    Debug.Log("Clicked on Harbor");
+                    //Debug.Log("Clicked on Harbor");
                     //Set Taskboard-cam active
-                    mainCam.SetActive(false);
-                    HarborCam.SetActive(true);
+                    ActivateCamera(HarborCam);
 
                     //Disable camera control
                     cameraControllerScript.enabled = false;
                 }
                 if (hit.transform.name == "CityHall")
                 {
-                    Debug.Log("Clicked on City Hall");
+                    //Debug.Log("Clicked on City Hall");
                     //Set Taskboard-cam active
-                    mainCam.SetActive(false);
-                    CityHallCam.SetActive(true);
+                    ActivateCamera(CityHallCam);
 
                     //Disable camera control
                     cameraControllerScript.enabled = false;
                 }
                 if (hit.transform.name == "Hotel")
                 {
-                    Debug.Log("Clicked on Hotel");
+                    //Debug.Log("Clicked on Hotel");
                     //Set Taskboard-cam active
-                    mainCam.SetActive(false);
-                    HotelCam.SetActive(true);
+                    ActivateCamera(HotelCam);
 
                     //Disable camera control
                     cameraControllerScript.enabled = false;
                 }
                 if (hit.transform.name == "CowFarm")
                 {
-                    Debug.Log("Clicked on Cow Farm");
+                    //Debug.Log("Clicked on Cow Farm");
                     //Set Taskboard-cam active
                     mainCam.SetActive(false);
                     CowFarmCam.SetActive(true);
@@ -104,7 +96,7 @@ public class ClickManager : MonoBehaviour
                 }
                 if (hit.transform.name == "Busstation")
                 {
-                    Debug.Log("Clicked on Bus Station");
+                    //Debug.Log("Clicked on Bus Station");
                     //Set Taskboard-cam active
                     mainCam.SetActive(false);
                     BusStationCam.SetActive(true);
@@ -114,7 +106,7 @@ public class ClickManager : MonoBehaviour
                 }
                 if (hit.transform.name == "CropFarm")
                 {
-                    Debug.Log("Clicked on Crop Farm");
+                    //Debug.Log("Clicked on Crop Farm");
                     //Set Taskboard-cam active
                     mainCam.SetActive(false);
                     CropFarmCam.SetActive(true);
@@ -124,7 +116,7 @@ public class ClickManager : MonoBehaviour
                 }
                 if (hit.transform.name == "MerchFactory")
                 {
-                    Debug.Log("Clicked on Merch Factory");
+                    //Debug.Log("Clicked on Merch Factory");
                     //Set Taskboard-cam active
                     mainCam.SetActive(false);
                     MerchFactoryCam.SetActive(true);
@@ -146,6 +138,7 @@ public class ClickManager : MonoBehaviour
         SetCamInactive(CropFarmCam);
         SetCamInactive(MerchFactoryCam);
     }
+
 
     private bool DoubleClick()
     {
@@ -173,13 +166,19 @@ public class ClickManager : MonoBehaviour
         return false;
     }
 
+    void ActivateCamera(GameObject camera)
+    {
+        mainCam.SetActive(false);
+        camera.SetActive(true);
+    }
+
     private void SetCamInactive(GameObject camera)
     {
         if (camera.activeSelf == true)
         {
             if (Input.GetKey(KeyCode.Escape))
             {
-                Debug.Log("Escape-key pressed");
+                //Debug.Log("Escape-key pressed");
                 camera.SetActive(false);
                 mainCam.SetActive(true);
                 cameraControllerScript.enabled = true;
