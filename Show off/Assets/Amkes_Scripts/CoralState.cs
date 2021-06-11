8b70423f4ea21b411f1dabe1eb23612ac2c694d1
Amke Dijkstra <amkedijkstra@hotmail.com>
2021-06-11 15:00:10 +0200

    Updates live the coral-state depending on coralHealth-variable

--- Show off/Assets/Amkes_Scripts/CoralState.cs
+++ Show off/Assets/Amkes_Scripts/CoralState.cs
@@ -8,6 +8,15 @@ public class CoralState : MonoBehaviour
 {
     public Text healthText;
     public Image healthImage;
+    public Material coral1Material;
+    public Material coral2Material;
+    public Material coral3Material;
+    public Material coral4Material;
+    public float numberDead;
+    public float numberOnTheBrink;
+    public float numberFairlyDamaged;
+    public float numberFairlyHealthy;
+    public float numberThriving;
 
     public enum CoralStates
     {
@@ -90,24 +99,40 @@ public class CoralState : MonoBehaviour
     {
         //Green
         healthImage.color = new Vector4(0, 1, 0, 1);
+        coral1Material.SetFloat("CoralHealth", numberThriving);
+        coral2Material.SetFloat("CoralHealth", numberThriving);
+        coral3Material.SetFloat("CoralHealth", numberThriving);
+        coral4Material.SetFloat("CoralHealth", numberThriving);
     }
 
     void SetColorFairlyHealthy()
     {
         //Yellow
         healthImage.color = new Vector4(1, 0.92f, 0.016f, 1);
+        coral1Material.SetFloat("CoralHealth", numberFairlyHealthy);
+        coral2Material.SetFloat("CoralHealth", numberFairlyHealthy);
+        coral3Material.SetFloat("CoralHealth", numberFairlyHealthy);
+        coral4Material.SetFloat("CoralHealth", numberFairlyHealthy);
     }
 
     void SetColorFairlyDamaged()
     {
         //Orange
         healthImage.color = new Vector4(1, 0.6f, 0, 1);
+        coral1Material.SetFloat("CoralHealth", numberFairlyDamaged);
+        coral2Material.SetFloat("CoralHealth", numberFairlyDamaged);
+        coral3Material.SetFloat("CoralHealth", numberFairlyDamaged);
+        coral4Material.SetFloat("CoralHealth", numberFairlyDamaged);
     }
 
     void SetColorOnTheBrink()
     {
         //Red
         healthImage.color = new Vector4(1, 0, 0, 1);
+        coral1Material.SetFloat("CoralHealth", numberOnTheBrink);
+        coral2Material.SetFloat("CoralHealth", numberOnTheBrink);
+        coral3Material.SetFloat("CoralHealth", numberOnTheBrink);
+        coral4Material.SetFloat("CoralHealth", numberOnTheBrink);
     }
 
     void SetColorDead()
@@ -115,5 +140,9 @@ public class CoralState : MonoBehaviour
         //Black
         healthImage.color = new Vector4(0, 0, 0, 1);
         healthText.color = Color.white;
+        coral1Material.SetFloat("CoralHealth", numberDead);
+        coral2Material.SetFloat("CoralHealth", numberDead);
+        coral3Material.SetFloat("CoralHealth", numberDead);
+        coral4Material.SetFloat("CoralHealth", numberDead);
     }
 }
