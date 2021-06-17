dd9311d36f03def65cce12fa51869dc515df5608
LucRoosjen <luc.roosjen@gmail.com>
2021-06-17 14:05:33 +0200

    Extended Hud Info + some language changes

--- Show off/Assets/Scripts/ui/HoverState.cs
+++ Show off/Assets/Scripts/ui/HoverState.cs
@@ -9,12 +9,12 @@ public class HoverState : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
     public void OnPointerEnter(PointerEventData pointerEventData)
     {
         HoverSprite.SetActive(true);
-        Debug.Log("Cursor Entering " + name + " GameObject");
+        //Debug.Log("Cursor Entering " + name + " GameObject");
     }
 
     public void OnPointerExit(PointerEventData pointerEventData)
     {
         HoverSprite.SetActive(false);
-        Debug.Log("Cursor Exiting " + name + " GameObject");
+        //Debug.Log("Cursor Exiting " + name + " GameObject");
     }
 }
