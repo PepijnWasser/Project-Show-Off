48417b42f0fcbf3897faf5dc151792ba1aa1fba1
Amke Dijkstra <amkedijkstra@hotmail.com>
2021-06-18 11:19:49 +0200

    Adds dynamic difficulty

--- Show off/Assets/Amkes_Scripts/DynamicDifficulty.cs
+++ Show off/Assets/Amkes_Scripts/DynamicDifficulty.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class DynamicDifficulty : MonoBehaviour
@@ -10,13 +11,27 @@ public class DynamicDifficulty : MonoBehaviour
 
     private void Update()
     {
-        if(timeScript.dayNumber == 3 || timeScript.dayNumber == 6)
+        if (timeScript.dayNumber == 3 || timeScript.dayNumber == 6)
         {
+            float healthScore = Int32.Parse(coralStateScript.healthText.text);
+
             //Coralhealth <=6           --> 3 good, 1 bad
+            if (healthScore <= 6)
+            {
+                taskManagerScript.minMaxPositiveCoralTask = new Vector2(3, 3);
+            }
 
             //Coralhealth >6 && <9      --> nothing changes
+            if (healthScore > 6 && healthScore < 9)
+            {
+                taskManagerScript.minMaxPositiveCoralTask = new Vector2(1, 2);
+            }
 
             //Coralhealth >= 9          --> 1 good, 3 bad
+            if (healthScore >= 9)
+            {
+                taskManagerScript.minMaxPositiveCoralTask = new Vector2(1, 1);
+            }
         }
     }
 }
