4ae13bd3753943728c4ae59bbc2ba3436b4818af
Amke Dijkstra <amkedijkstra@hotmail.com>
2021-06-30 22:16:34 +0200

    Writes down tutorial lines in chosen language on mouse-click

--- Show off/Assets/Scripts/Amkes_Scripts/ReadInText.cs
+++ Show off/Assets/Scripts/Amkes_Scripts/ReadInText.cs
@@ -10,19 +10,43 @@ public class ReadInText : MonoBehaviour
     [SerializeField] private LanguageIconManager languageIconManagerScript;
     private string[] dutchLines;
     private string[] englishLines;
+    private int counter;
 
     private void Start()
     {
+        counter = 0;
+
         dutchLines = File.ReadAllLines("Assets/Amkes_Files/A_Txt/TutorialNederlands.txt");
         englishLines = File.ReadAllLines("Assets/Amkes_Files/A_Txt/TutorialEnglish.txt");
 
+        WriteCurrentLine();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            counter++;
+
+            if (counter >= englishLines.Length || counter >= dutchLines.Length)
+            {
+                counter = 0;
+            }
+            Debug.Log("Counter: " + counter);
+
+            WriteCurrentLine();
+        }
+    }
+
+    private void WriteCurrentLine()
+    {
         if (languageIconManagerScript.startLanguage == "EN")
         {
-            bubbleText.text = englishLines[0];
+            bubbleText.text = englishLines[counter];
         }
         else if (languageIconManagerScript.startLanguage == "NL")
         {
-            bubbleText.text = dutchLines[0];
+            bubbleText.text = dutchLines[counter];
         }
     }
 }
